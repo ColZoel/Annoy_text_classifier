115cd7da5b21633f700511405db38bab1b7d8994
ColZoel <105737771+ColZoel@users.noreply.github.com>
2025-03-26 15:16:05 -0400

    pull pipeline from modules

--- prototype.py
+++ prototype.py
@@ -1,29 +1,73 @@
 import pandas as pd
 
 from modules import *
-from data import *
+from tools.data import *
+
+
+def training():
+    data = pd.read_csv('data/eval.csv')
+    data['title'] = clean_data(data['title'])
+    data.to_csv('data/eval.csv', index=False)
+    return data
+
+
+def get_embeddings(model, data):
+
+    labels = pd.read_csv('data/occupations_workathome.csv')['title'].to_numpy()
+    target_embeddings = embed(model, labels)
+    feature_embeddings = embed_batched(model, data['title'].to_numpy())
+
+    save_embeddings(feature_embeddings, 'embeds/features.npz')
+    save_embeddings(target_embeddings, 'embeds/target.npz')
+
+    return target_embeddings, feature_embeddings
+
+
+def pipeline(model: str, labels: np.array, data: np.array,
+             num_trees: int, num_neighbors: int,
+             batch_size: int):
+
+    # 1. Load pre-trained model
+    model = SentenceTransformer(model)
+
+    # 2. Target and feature embeddings
+    target_embeddings, feature_embeddings = get_embeddings(model, data)
+
+    # 3. Build Annoy Index for target classes
+    tree = build_tree(target_embeddings, num_trees=num_trees)
+
+    # 4. Predict labels
+    yhat = predict_labels(tree, labels, feature_embeddings, neighbors=num_neighbors)
+
+    return target_embeddings, feature_embeddings, yhat
+
+
+
+
 
 def main():
     onet_classes = pd.read_csv(r"data/occupations_workathome.csv")['title']
     onet_classes = onet_classes.to_numpy()
+    y, X = read_eval_data('data/eval.csv')
 
-    k = pd.read_parquet("data/kaggle_clean_data.parquet")
-    r = pd.read_parquet("data/reddit_data.parquet")
-    k =k.rename(columns={"job_title": "title"})
-    data = pd.concat([k, r]).to_numpy()
-    # true_values, noisy_data = make_random_data(onet_classes, num_obs=1000000)
-
-    yhat = pipeline(
+    y_embed, x_embed, yhat = pipeline(
             model="all-MiniLM-L6-v2",
             labels=onet_classes,
-            data=data,
+            data=X,
             num_trees=5000,
-            num_neighbors=10,
+            num_neighbors=1,
             batch_size=1024,
-            save_fig=True
         )
 
-    # evaluate(true_values, yhat)
+    visualize(x_embed, yhat, y_embed, onet_classes,
+              top=5,
+              label_points=True,
+              figsize=(6, 6),
+              save="100_small_samples.png")
+
+    save_pred(X, yhat, "small_samples.csv")
+
+    evaluate(y, yhat)
 
 
 if __name__ == "__main__":
