d0c87f81b2f974f324edae4e247a3a8cab5afe22
ColZoel <105737771+ColZoel@users.noreply.github.com>
2025-03-26 15:15:31 -0400

    add timer, clean

--- modules.py
+++ modules.py
@@ -11,32 +11,87 @@ from annoy import AnnoyIndex
 from sklearn.metrics import classification_report
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import os
 
 
-def embed_batched(model, data, batch_size=1000):
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(f"(finished in {time.time() - start:.2f} seconds, "
+              f"avg: {(time.time() - start) / len(args[0]):.4f} sec/label)")
+        return result
+    return wrapper
+
+# ################################################## EMBEDDINGS ##################################################
+
+
+@timer
+def embed(model: SentenceTransformer, data: np.array):
+    """Embeddings for the given data."""
+
+    print(f"\nEncoding {len(data)} target label values")
+    embeddings = model.encode(data, convert_to_numpy=True, show_progress_bar=True)
+    return embeddings
+
+
+@timer
+def embed_batched(model, data: np.array, batch_size: int = 1000) -> np.array:
     """embeddings in batches."""
 
     num_batches = (len(data) + batch_size - 1) // batch_size
+
+    print(f"\nEncoding {len(data)} target label values ({num_batches} batches)")
     batch_indices = np.array_split(np.arange(len(data)), num_batches)
 
-    embeds = np.vstack([model.encode(data[indices], convert_to_numpy=True, show_progress_bar=False) for indices in batch_indices])
+    embeddings = np.vstack([model.encode(data[indices],
+                                         convert_to_numpy=True,
+                                         show_progress_bar=False) for indices in batch_indices])
 
-    return embeds
+    return embeddings
 
 
-def build_tree(embeddings, num_trees=10):
+def save_embeddings(embeddings: np.array, path: str) -> None:
+    np.savez_compressed(path, **embeddings)
+    return None
+
+
+def load_embeddings(path: str) -> np.array:
+    return np.load(path)
+
+# ################################################### TREE  ####################################################
+
+
+@timer
+def build_tree(embeddings: np.array, num_trees: int = 10) -> AnnoyIndex:
     """Build an Annoy index for the given embeddings."""
+    print(f"\nBuilding Annoy index with {num_trees} trees")
     t = AnnoyIndex(embeddings.shape[1], 'euclidean')
     for i, emb in enumerate(embeddings):
         t.add_item(i, emb)
     t.build(num_trees)
-    print("Annoy index built with", num_trees, "trees.")
+
     return t
 
 
-def predict_labels(tree, classes, embeddings, neighbors=1):
-    """Predict the nearest neighbor labels for the given embeddings."""
+# ################################################### PREDICT ##################################################
+
+@timer
+def predict_labels(tree: AnnoyIndex,
+                   classes: [str, np.array],
+                   embeddings: np.array,
+                   neighbors: int = 1) -> np.array:
+    """Predict the nearest neighbor labels for the given embeddings.
+    Uses majority voting to determine the most common label among the neighbors.
+    :param tree: Built Annoy index
+    :param classes: Class labels
+    :param embeddings: Embeddings to predict
+    :param neighbors: Number of neighbors to consider
+    :return: Predicted labels
+    """
     # Retrieve neighbor indices for each embedding
+
+    print(f"\nPredicting labels for {len(embeddings)} embeddings")
     indices = [tree.get_nns_by_vector(emb.tolist(), neighbors) for emb in embeddings]
 
     # Map indices to their corresponding class labels
@@ -48,8 +103,20 @@ def predict_labels(tree, classes, embeddings, neighbors=1):
     return np.array(most_common_labels)
 
 
-def evaluate(y_true, y_pred):
-    """Evaluate the classification performance."""
+def save_pred(x: np.array, yhat: np.array, path: str) -> None:
+
+    df = pd.DataFrame([x, yhat], columns=['label', 'yhat'])
+    df.to_csv(path)
+    return
+
+
+#   ################################################### EVALUATE ##################################################
+def evaluate(y_true: np.array, y_pred: np.array) -> None:
+    """Evaluate the classification performance.
+
+    :param y_true: True labels
+    :param y_pred: Predicted labels
+    """
     comp = pd.DataFrame({"label": y_true, "yhat": y_pred})
     comp['correct'] = comp['label'] == comp['yhat']
     accuracy = comp['correct'].mean()
@@ -67,8 +134,27 @@ def evaluate(y_true, y_pred):
     return
 
 
-def visualize(x_embeddings, x_labels, class_embeddings, class_labels, save: bool = False):
-    """Visualize the embeddings using PCA."""
+def visualize(x_embeddings: np.array,
+              x_labels: [list, np.array],
+              class_embeddings: np.array,
+              class_labels: [list, np.array],
+              top: int = 10,
+              label_points: bool = False,
+              figsize=(10, 10),
+              save: str = None) -> None:
+
+    """Visualize the embeddings using PCA.
+    :param x_embeddings: Embeddings for the input data
+    :param x_labels: Labels for the input data
+    :param class_embeddings: Embeddings for the target classes
+    :param class_labels: Labels for the target classes
+    :param top: Number of top classes to display
+    :param label_points: Label the target class points
+    :param figsize: Figure size
+    :param save: Save the plot to a file
+    :return: None
+
+    """
     x_embeddings = np.vstack(x_embeddings)
     x_labels = np.array(x_labels)
     class_embeddings = np.vstack(class_embeddings)
@@ -79,69 +165,36 @@ def visualize(x_embeddings, x_labels, class_embeddings, class_labels, save: bool
     pca_class_embed = pca.transform(class_embeddings)
 
     unique_classes, counts = np.unique(x_labels, return_counts=True)
-    top_20_indices = np.argsort(-counts)[:20]
-    top_5_indices = np.argsort(-counts)[:5]
-    unique_classes_20 = unique_classes[top_20_indices]
-    unique_classes_5 = unique_classes[top_5_indices]
+    top_n_indices = np.argsort(-counts)[:top]
+    n_unique_classes = unique_classes[top_n_indices]
 
     colors = plt.colormaps['tab20']
-    class_to_color = {cls: colors(i) for i, cls in enumerate(unique_classes_20)}
+    class_to_color = {cls: colors(i) for i, cls in enumerate(n_unique_classes)}
 
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=figsize)
 
     # plot input embeddings
-    for cls in unique_classes_20:
+    for cls in n_unique_classes:
         cls_idx = np.where(x_labels == cls)[0]
         print(f"Class: {cls}, Count: {len(cls_idx)}")
         plt.scatter(pca_embed[cls_idx, 0], pca_embed[cls_idx, 1],
                     color=class_to_color[cls], alpha=0.6, marker='o')
 
     # Label classes
-    for cls in unique_classes_20:
+    for cls in n_unique_classes:
         cls_idx = np.where(class_labels == cls)[0]
         plt.scatter(pca_class_embed[cls_idx, 0], pca_class_embed[cls_idx, 1],
                     color=class_to_color[cls], label=f"{cls}", alpha=1.0, marker='x', s=250)
-        # for idx in cls_idx:
-        #     plt.text(pca_class_embed[idx, 0], pca_class_embed[idx, 1], cls, fontsize=9, ha='center', va='bottom')
+        plt.title(f"Embedding Clusters: Top {top} Occupations")
 
-    plt.title("Embedding Clusters in 2D: Top 20 Occupations")
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+        if label_points:
+            for idx in cls_idx:
+                plt.text(pca_class_embed[idx, 0], pca_class_embed[idx, 1], cls, fontsize=9, ha='center', va='bottom')
+
+    if not label_points:
+        plt.legend(loc='upper right')
+
     plt.show()
-    if save:
-        plt.savefig("embeddings.png")
+    if save is not None:
+        plt.savefig(save)
     return
-
-
-def pipeline(model: str, labels: np.array, data: np.array, num_trees: int, num_neighbors: int, batch_size: int, save_fig: bool = False):
-
-    # 1. Load pre-trained model
-    model = SentenceTransformer(model)
-
-    # 2. Encode target classes
-    enc_start = time.time()
-    print(f"Encoding {len(labels)} target label values")
-    target_embeddings = model.encode(labels, convert_to_numpy=True, show_progress_bar=True)
-    print(f"(finished in {time.time() - enc_start:.2f} seconds, avg: {(time.time() - enc_start) / len(labels):.4f} sec/label)")
-
-    # 3. Build Annoy Index for target classes
-    tree_start = time.time()
-    print(f"Building Annoy index with {num_trees} trees")
-    tree = build_tree(target_embeddings, num_trees=num_trees)
-    print(f"(finished in {time.time() - tree_start:.2f} seconds, avg: {(time.time() - tree_start) / len(labels):.4f} sec/label)")
-
-    # 4. Encode feature space and classify
-    features_start = time.time()
-    print(f"Encoding {len(data)} feature vectors")
-    feature_embeddings = embed_batched(model, data, batch_size=batch_size)
-    print(f"(finished in {time.time() - features_start:.2f} seconds, avg: {(time.time() - features_start) / len(data):.4f} sec/label)")
-
-    # 5. Predict labels
-    pred_start = time.time()
-    print("Predicting labels")
-    yhat = predict_labels(tree, labels, feature_embeddings, neighbors=num_neighbors)
-    print(f"(finished in {time.time() - pred_start:.2f} seconds, avg: {(time.time() - pred_start) / len(data):.4f} sec/label)")
-
-    # 6. Visualize
-    visualize(feature_embeddings, yhat, target_embeddings, labels, save=save_fig)
-
-    return yhat
